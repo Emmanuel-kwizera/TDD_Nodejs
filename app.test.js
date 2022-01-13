905e804546b6f42d018b61db13bc944d24594826
Emmanuel-kwizera <kwizemmy2004@gmail.com>
2022-01-13 21:50:44 +0200

    intorducing user test blocks

--- app.test.js
+++ app.test.js
@@ -1,3 +1,13 @@
 import supertest from 'supertest'
+import { describe } from 'yargs'
 import app from "./app"
 
+describe("POST /users", () =>{
+    describe("given username and password", () => {
+
+    })
+
+    describe("when username and password is missing", () => {
+        
+    })
+})
