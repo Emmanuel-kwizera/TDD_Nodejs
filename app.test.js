385b5dc949f342303f288544f44f8cdeac643cd4
Emmanuel-kwizera <kwizemmy2004@gmail.com>
2022-01-13 21:57:01 +0200

    adding the first test

--- app.test.js
+++ app.test.js
@@ -1,13 +1,21 @@
+import { expect } from '@jest/globals'
+import { request } from 'http'
 import supertest from 'supertest'
 import { describe } from 'yargs'
 import app from "./app"
 
 describe("POST /users", () =>{
     describe("given username and password", () => {
-
+        test('should respond with a 200 status code', () => {
+            const response = await request(app).post("/users").send({
+                username: "username",
+                password: "password"       
+            })
+            expect(response.statusCode).toBe(200)
+        })        
     })
 
     describe("when username and password is missing", () => {
-        
+
     })
 })
