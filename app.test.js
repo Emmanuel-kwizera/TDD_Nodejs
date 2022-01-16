b7693ebc764e0074bf56cd71c18876faaeeba431
Emmanuel-kwizera <kwizemmy2004@gmail.com>
2022-01-16 19:33:23 +0200

    changin the test

--- app.test.js
+++ app.test.js
@@ -1,28 +1,49 @@
-// import { expect } from '@jest/globals'
-import { request } from 'http'
-import supertest from 'supertest'
-import { describe } from 'yargs'
-import app from "./app"
+import request from 'supertest'
+import makeApp from './app.js'
+import { jest } from '@jest/globals'
 
-describe("POST /users", () =>{
-    describe("given username and password", () => {
-        test('should respond with a 200 status code', async () => {
-            const response = await request(app).post("/users").send({
-                username: "username",
-                password: "password"       
-            })
-            expect(response.statusCode).toBe(200)
-        })    
-        test('should specify json in the content type header', async () => {
-            const response = await request(app).post("/users").send({
-                username: "username",
-                password: "password"       
-            })
-            expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
-        })
-            
-    })
+const createUser = jest.fn()
+const getUser = jest.fn()
 
-    describe("when username and password is missing", () => {
+const app = makeApp({
+  createUser,
+  getUser
+})
+
+describe("POST /users", () => {
+
+  beforeEach(() => {
+    createUser.mockReset()
+    createUser.mockResolvedValue(0)
+  })
+
+  describe("given a username and password", () => {
+
+    test("should respond with a 200 status code", async () => {
+      const response = await request(app).post("/users").send({
+        username: "username",
+        password: "password"
+      })
+      expect(response.statusCode).toBe(200)
     })
+    test("should specify json in the content type header", async () => {
+      const response = await request(app).post("/users").send({
+        username: "username",
+        password: "password"
+      })
+      expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
+    })
+    test("response has userId", async () => {
+      const response = await request(app).post("/users").send({
+        username: "username",
+        password: "password"
+      })
+      expect(response.body.userId).toBeDefined()
+    })
+  })
+
+  describe("when the username and password is missing", () => {
+    
+  })
+
 })
