28c40a9130a06b0d8a3c4054171230f5f29d2af9
Emmanuel-kwizera <kwizemmy2004@gmail.com>
2022-01-16 19:41:56 +0200

    a getter

--- database.js
+++ database.js
@@ -5,3 +5,14 @@ const connection = mysql.createPool({
   user     : 'root',
   database : 'some_database'
 })
+
+export async function getUser(username) {
+    const [rows] = await connection.promise().query(
+      `SELECT * 
+        FROM users 
+        WHERE username = ?`,
+      [username]
+    )
+  
+    return rows[0]
+  }
